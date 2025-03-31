94e9e776fb4e968ede52df7f6d82b1461e0c257a
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-03-31 21:02:58 +0330

    update title

--- a0507.py
+++ a0507.py
@@ -7,7 +7,7 @@ from utils.date import is_holiday
 SEP = ';'
 ENCODING = 'utf-8'
 
-st.title("A05_07: List Noll Bearb")
+st.title("A05_07: List Noll Bearb, A05_08: List Noll Verkställ")
 
 uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 # icons source: https://fonts.google.com/icons
 
 pg = st.navigation([
-  st.Page("a0507.py", title="A05_07: List Noll Bearb"),
+  st.Page("a0507.py", title="A05_07: List Noll Bearb, A05_08: List Noll Verkställ"),
   # st.Page("a0202.py", title="A02_02: Sjuk o frånvaro med omf"),
   st.Page("s210.py", title="S2_10:Sjuk mer än 365 dagar"),
 ])
